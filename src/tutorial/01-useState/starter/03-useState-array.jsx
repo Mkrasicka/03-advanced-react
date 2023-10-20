a10463e893ab594058085798ecbd316ccbae785f
magdalena krasicka <krasickamagdalena@gmail.com>
2023-10-20 17:41:38 +0100

    using state array hooks

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,40 @@
+import { data } from "../../../data.js";
+import React from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  // !set array as people
+  const [people, setPeople] = React.useState(data);
+
+  const removeItem = () => {};
+  // !set array to an empty array
+  const clearAllPeople = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div>
+      {/* //!Iterate through people and set props to person */}
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type="button" className="btn">
+              remove
+            </button>
+          </div>
+        );
+      })}
+      <button
+        type="button"
+        className="btn"
+        style={{ marginTop: "2rem" }}
+        onClick={clearAllPeople}
+      >
+        Remove all
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
