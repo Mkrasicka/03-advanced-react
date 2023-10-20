963666bed34b10f795377e7e8b5d3b1f28541bf9
magdalena krasicka <krasickamagdalena@gmail.com>
2023-10-20 16:25:45 +0100

    hooks

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Starter from "./tutorial/01-useState/starter/02-useState-basics";
+
+function App() {
+  return (
+    <div className="container">
+      <h2>HOOKS</h2>
+      <Starter />
+    </div>
+  );
+}
+
+export default App;
