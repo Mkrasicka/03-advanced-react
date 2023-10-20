446a5ea1da04516b429834e091fdef47485c8557
magdalena krasicka <krasickamagdalena@gmail.com>
2023-10-20 19:31:47 +0100

    value is synchronized

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setValue((currentValue) => {
+      currentValue = value + 1;
+      console.log(currentValue);
+      return currentValue;
+    });
+  };
+
+  return (
+    <div>
+      <h1>Clicked : {value} times</h1>
+      <button className="btn" type="button" onClick={handleClick}>
+        Increase
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
